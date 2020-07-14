7b545712e444bedb981d177512aa7196c679898e
Khoirur Rozaq <rozaqarmedian@gmail.com>
2020-07-14 18:01:53 +0700

    add expired

--- resources/views/emails/konfirmasi.blade.php
+++ resources/views/emails/konfirmasi.blade.php
@@ -1,8 +1,8 @@
 Konfirmasi Pembayaran {{ $name }}
 <br><br>
 
-    Nama Rekening Pengirim : {{ $nama_rekening }}
-    No Rekening Pengirim : {{ $no_rekening }}
-    Catatan : {{ $catatan }}
+    Nama Rekening Pengirim : {{ $nama_rekening }} <br>
+    No Rekening Pengirim : {{ $no_rekening }} <br>
+    Catatan : {{ $catatan }} <br>
 
     <br>
